c80b6cadd9068c395fd2129023230744c5770994
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-04-01 22:19:50 +0300

    add post method specialities

--- src/http/SpecialitiesApi.js
+++ src/http/SpecialitiesApi.js
@@ -4,3 +4,8 @@ export const getSpecialities = async () =>{
     const {data} = await $authHost.get("/api/specializations")
     return data;
 }
+
+export const addSpeciality = async (name) =>{
+    const {data} = await $authHost.post('/api/spicializations', name)
+    return data;
+}
